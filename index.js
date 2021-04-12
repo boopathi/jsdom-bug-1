5e787858a7694fa5288541c1b73fa5e91b9b6ae1
Boopathi Rajaa <me@boopathi.in>
2021-04-12 23:58:31 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,42 @@
+"use strict";
+
+const { PurgeCSS } = require("purgecss");
+const fromHtml = require("purgecss-from-html");
+
+// removing this require will succeed running this file
+require("jsdom");
+
+const html = `
+<html>
+  <head>
+    <style>
+    .foo { background: blue; }
+    .bar { color: black; }
+    </style>
+  </head>
+  <body><div class="foo">Hello World</div></body>
+</html>
+`;
+
+async function main() {
+  const purger = new PurgeCSS();
+
+  const results = await purger.purge({
+    content: [
+      {
+        raw: html,
+        extension: "html",
+      },
+    ],
+    extractors: [
+      {
+        extractor: fromHtml,
+        extensions: ["html"],
+      },
+    ],
+  });
+
+  console.log(results);
+}
+
+main().then(() => console.log("done"));
